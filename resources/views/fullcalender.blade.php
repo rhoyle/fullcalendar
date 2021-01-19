289acf527cc15481c004c6a5167aa5b004cbd00a
Richard A. Hoyle <rhoyle@comcast.net>
2021-01-19 11:27:56 -0600

    the system has been compiled

--- resources/views/fullcalender.blade.php
+++ resources/views/fullcalender.blade.php
@@ -17,7 +17,23 @@
 
 <div class="container">
     <h1>Iron Man Recycling</h1>
+
+    @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <a href="/" class="ml-4 text-sm text-gray-700 underline">Home</a>
+            @auth
+                <a href="{{ url('/home') }}" class="ml-4 text-sm text-gray-700 underline">Home</a>
+            @else
+                <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 underline">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
     <div id='calendar'></div>
+
 </div>
 
 <script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Full Calendar</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -22,12 +22,14 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <a href="/fullcalender" class="ml-4 text-sm text-gray-700 underline">Calendar</a>
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="ml-4 text-sm text-gray-700 underline">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                        <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 underline">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
